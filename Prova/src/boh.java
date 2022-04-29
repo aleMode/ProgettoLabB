497bbc7afa3c3728cba36a1d13c671e694717c7b
ale <ale@gesoo>
2022-04-30 00:51:33 +0200

    Primo commit

---
+++ Prova/src/boh.java
@@ -0,0 +1,48 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class boh extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtText;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					boh frame = new boh();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public boh() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtText = new JTextField();
+		txtText.setText("text");
+		txtText.setBounds(283, 48, 114, 21);
+		contentPane.add(txtText);
+		txtText.setColumns(10);
+	}
+
+}
