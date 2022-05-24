1b015642bdf0c98807d0105df3eae1fc6f6ae426
aleMode <103491521+aleMode@users.noreply.github.com>
2022-05-24 12:03:14 +0200

    Sistemata OSPage per esistere nel layout

--- Prova/src/OSPage.java
+++ Prova/src/OSPage.java
@@ -1,4 +1,7 @@
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -7,48 +10,23 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
-public class OperaOperatore extends JFrame {
+public class OSPage extends JPanel {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+		private JPanel contentPane;
 	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					OperaOperatore frame = new OperaOperatore();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
+	public OSPage(JPanel cardStack) {
 
-	/**
-	 * Create the frame.
-	 */
-	public OperaOperatore() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane = cardStack;
 		
+		setLayout(new BorderLayout());
 		JButton btnNewButton = new JButton("Registra");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(160, 109, 117, 29);
-		contentPane.add(btnNewButton);
+		btnNewButton.setBounds(260, 109, 117, 29);
+		add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Exit");
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -57,7 +35,7 @@ public class OperaOperatore extends JFrame {
 			}
 		});
 		btnNewButton_1.setBounds(6, 6, 75, 29);
-		contentPane.add(btnNewButton_1);
+		add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Registra");
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -65,18 +43,18 @@ public class OperaOperatore extends JFrame {
 			}
 		});
 		btnNewButton_2.setBounds(160, 202, 117, 29);
-		contentPane.add(btnNewButton_2);
+		add(btnNewButton_2);
 		
 		JLabel lblNewLabel = new JLabel("Cosa vuoi fare?");
 		lblNewLabel.setBounds(170, 37, 117, 16);
-		contentPane.add(lblNewLabel);
+		add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Registrare un nuovo centro vaccinale");
 		lblNewLabel_1.setBounds(105, 81, 261, 16);
-		contentPane.add(lblNewLabel_1);
+		add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Registrare una nuova vaccinazione");
 		lblNewLabel_2.setBounds(105, 174, 261, 16);
-		contentPane.add(lblNewLabel_2);
+		add(lblNewLabel_2);
 	}
 }
