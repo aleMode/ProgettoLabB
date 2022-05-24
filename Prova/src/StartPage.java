f044cb1e45c088e77c48393aa57eac2a6b15ff17
aleMode <103491521+aleMode@users.noreply.github.com>
2022-05-24 12:03:44 +0200

    Sistemata StartPage per esistere nel layout

--- Prova/src/StartPage.java
+++ Prova/src/StartPage.java
@@ -5,7 +5,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.Font;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.GroupLayout;
@@ -17,77 +19,50 @@ public class StartPage extends JPanel {
 	
 	
 	public StartPage(JPanel cardStack) {
-		setBounds(100, 100, 550, 300);
 		contentPane = cardStack;
-		contentPane.setBackground(new Color(153, 255, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		setBackground(new Color(153, 255, 255));
 		
 		JButton CittButton = new JButton("Cittadini");
 		CittButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {}
-				
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+				cardLayout.show(contentPane,"CittPage");
 			}
-		);
+				
+		});
 		CittButton.setBackground(new Color(51, 153, 204));
 		CittButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		add(CittButton);
 		
 		JButton OpSButton = new JButton("Operatore sanitario");
 		OpSButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == OpSButton) { 
-				   
-			}}
+				CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+				cardLayout.show(contentPane,"OSPage");
+			}
 		});
 		
 		OpSButton.setBackground(new Color(51, 153, 204));
 		OpSButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		
+		add(OpSButton);
+
 		JTextArea txtrIndica = new JTextArea();
 		txtrIndica.setBackground(new Color(153, 255, 255));
 		txtrIndica.setFont(new Font("Calibri", Font.BOLD, 25));
 		txtrIndica.setText("Indica come vuoi operare:");
+		add(txtrIndica);
 		
 		JTextArea txtrBenvenuto = new JTextArea();
 		txtrBenvenuto.setBackground(new Color(153, 255, 255));
 		txtrBenvenuto.setForeground(new Color(0, 0, 0));
 		txtrBenvenuto.setFont(new Font("Calibri", Font.BOLD, 60));
 		txtrBenvenuto.setText("Benvenuto!");
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(105)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(11)
-							.addComponent(txtrIndica, GroupLayout.PREFERRED_SIZE, 301, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(txtrBenvenuto)
-							.addGap(16)))
-					.addGap(107))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(54)
-					.addComponent(CittButton, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-					.addGap(45)
-					.addComponent(OpSButton, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-					.addGap(65))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(20)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(63)
-							.addComponent(txtrIndica, GroupLayout.PREFERRED_SIZE, 26, Short.MAX_VALUE)
-							.addGap(24))
-						.addComponent(txtrBenvenuto, GroupLayout.DEFAULT_SIZE, 113, Short.MAX_VALUE))
-					.addGap(5)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(CittButton, GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
-						.addComponent(OpSButton, GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE))
-					.addGap(33))
-		);
-		contentPane.setLayout(gl_contentPane);
+		add(txtrBenvenuto);
+		
 	}
+	 @Override
+	 public Dimension getPreferredSize(){
+		 return (new Dimension(500, 500));
+	 }
 }
