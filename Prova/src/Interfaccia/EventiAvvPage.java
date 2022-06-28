9c1921cb80d2a7cfd304fa859519a0dd39279547
Ferri-Matteo <mferri3@studenti.uninsubria.it>
2022-06-28 12:00:09 +0200

    fine

--- Prova/src/Interfaccia/EventiAvvPage.java
+++ Prova/src/Interfaccia/EventiAvvPage.java
@@ -2,67 +2,125 @@ package Interfaccia;
 
 import java.awt.CardLayout;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JList;
 import javax.swing.JComboBox;
-import javax.swing.JEditorPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
+import java.awt.Dimension;
 
 public class EventiAvvPage extends JPanel {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	public EventiAvvPage(JPanel cardStack) {
+		
 		contentPane = cardStack;
 		
-		JButton btnBack = new JButton("Back");
+		setBackground(new Color(153, 255, 255));
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		JButton btnBack = new JButton("Indietro");
+		btnBack.setBackground(new Color(255, 255, 204));
+		btnBack.setFont(new Font("Calibri", Font.PLAIN, 11));
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CardLayout cardLayout = (CardLayout) contentPane.getLayout();
 				cardLayout.show(contentPane,"loginEventiAvv");
 			}
 		});
-		add(btnBack);
+		c.gridx=0;
+		c.gridy=0;
+		add(btnBack, c);
 		
-		JLabel labelInserisciDati = new JLabel("Inserisci i dati richiesti");
-		labelInserisciDati.setFont(new Font("Dialog", Font.PLAIN, 14));
-		add(labelInserisciDati);
+		JLabel labelInserisciDati = new JLabel();
+		labelInserisciDati.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		labelInserisciDati.setBackground(new Color(51, 153, 204));
+		labelInserisciDati.setText("Inserisci i dati richiesti:");
+		c.gridx=1;
+		c.gridy=1;
+		c.gridwidth = 2;
+		c.weighty = 0.1;
+		add(labelInserisciDati, c);
+		c.gridwidth = 1;
 		
-		JLabel lblEvento = new JLabel("Scegli un sintomo");
-		lblEvento.setFont(new Font("Dialog", Font.PLAIN, 14));
-		add(lblEvento);
+		JLabel lblEvento = new JLabel();
+		lblEvento.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblEvento.setBackground(new Color(51, 153, 204));
+		lblEvento.setText("Scegli un sintomo");
+		c.gridx=1;
+		c.gridy=2;
+		add(lblEvento,c);
 		
 		JComboBox comboBox = new JComboBox();
-		add(comboBox);
+		c.gridx=2;
+		c.gridy=2;
+		add(comboBox, c);
 		
-		JList list_1 = new JList();
-		add(list_1);
+        /*JList list_1 = new JList();
+		c.gridx=1;
+		c.gridy=2;
+		add(list_1, c);*/
 		
-		JLabel lblIntensita = new JLabel("Intensit√†");
-		lblIntensita.setFont(new Font("Dialog", Font.PLAIN, 14));
-		add(lblIntensita);
+		JLabel lblIntensita = new JLabel();
+		lblIntensita.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblIntensita.setBackground(new Color(51, 153, 204));
+		lblIntensita.setText("Intensit‡:");
+		c.gridx=1;
+		c.gridy=3;
+		add(lblIntensita,c);
 		
-		JLabel lblIntValue = new JLabel("1");
-		lblIntValue.setFont(new Font("Dialog", Font.PLAIN, 12));
-		add(lblIntValue);
-		
+		JLabel lblIntValue = new JLabel();
+		lblIntValue.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblIntensita.setBackground(new Color(51, 153, 204));
+		lblIntValue.setText("1");
+		c.gridx=2;
+		c.gridy=3;
+		add(lblIntValue, c);
+
 		JButton btnUp = new JButton("+");
-		add(btnUp);
+		btnUp.setBackground(new Color(51, 153, 204));
+		btnUp.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		c.gridx=3;
+		c.gridy=3;
+		add(btnUp, c);
 		
 		JButton btnDown = new JButton("-");
-		add(btnDown);
+		btnDown.setBackground(new Color(51, 153, 204));
+		btnDown.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		c.gridx=4;
+		c.gridy=3;
+		add(btnDown, c);
 		
-		JLabel lblNote = new JLabel("Note (max 256 caratteri)");
-		lblNote.setFont(new Font("Dialog", Font.PLAIN, 14));
-		add(lblNote);
+		JLabel lblNote = new JLabel();
+		lblNote.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNote.setBackground(new Color(51, 153, 204));
+		lblNote.setText("Note (max 256 caratteri)");
+		c.gridx=1;
+		c.gridy=4;
+		add(lblNote, c);
 		
-		JEditorPane editorPane = new JEditorPane();
-		editorPane.setBackground(Color.LIGHT_GRAY);
-		add(editorPane);
+		JTextPane Pane = new JTextPane();
+		c.gridx=1;
+		c.gridy=5;
+		c.gridwidth = 3;
+		c.gridheight = 3;
+		Pane.setBackground(Color.LIGHT_GRAY);
+		c.fill = GridBagConstraints.BOTH;
+		add(Pane, c);
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.fill = GridBagConstraints.NONE;
 		
 		JButton btnInviaFin = new JButton("Invia sintomo finale");
 		btnInviaFin.addActionListener(new ActionListener() {
@@ -72,12 +130,27 @@ public class EventiAvvPage extends JPanel {
 				cardLayout.show(contentPane,"finalPage");
 			}
 		});
-		add(btnInviaFin);
+		btnInviaFin.setBackground(new Color(51, 153, 204));
+		btnInviaFin.setPreferredSize(new Dimension(150,50));
+		c.gridx=4;
+		c.gridy=5;
+		c.insets = new Insets(0,5,0,5);
+		add(btnInviaFin, c);
 		
 		JButton btnInviaCont = new JButton("Invia e inserisci nuovo sintomo");
-		add(btnInviaCont);
+		btnInviaCont.setBackground(new Color(51, 153, 204));
+		btnInviaCont.setPreferredSize(new Dimension(150,50));
+		c.gridx=4;
+		c.gridy=6;
+		c.insets = new Insets(0,5,0,5);
+		add(btnInviaCont, c);
 		
 		JButton btnRipristina = new JButton("Ripristina");
-		add(btnRipristina);
+		btnRipristina.setBackground(new Color(51, 153, 204));
+		btnRipristina.setPreferredSize(new Dimension(150,50));
+		c.gridx=4;
+		c.gridy=7;
+		c.insets = new Insets(0,5,0,5);
+		add(btnRipristina, c);
 	}
 }
