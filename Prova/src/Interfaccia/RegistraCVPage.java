461f1c40d00cd6d01604cd870f09ab638678af06
aleMode <ale.modena1@gmail.com>
2022-07-24 18:37:53 +0200

    Fatta pagina 8

--- Prova/src/Interfaccia/RegistraCVPage.java
+++ Prova/src/Interfaccia/RegistraCVPage.java
@@ -8,6 +8,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -24,6 +26,7 @@ public class RegistraCVPage extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private String[] tipi = {"Hub","Ospedaliero","Aziendale"};
 	
 	public RegistraCVPage(JPanel cardStack, ServerMainInterface stub) {
 		 
@@ -49,12 +52,12 @@ public class RegistraCVPage extends JPanel {
 		c.weightx=1;
 		add(btnBack, c);
 		
-		JLabel lblNewLabel = new JLabel("Inserisci i dati richiesti");
+		JLabel lblInserisciDati = new JLabel("Inserisci i dati richiesti");
 		c.gridx=1;
 		c.gridy=0;
 		c.gridwidth=3;
 		c.insets= new Insets(5,5,5,5); 
-		add(lblNewLabel,c);
+		add(lblInserisciDati,c);
 		c.gridwidth=1;
 
 		
@@ -63,84 +66,116 @@ public class RegistraCVPage extends JPanel {
 		c.gridy=1;
 		add(lblNomeCentro,c);
 		
-		JTextField textField = new JTextField();
+		JTextField txfNome = new JTextField();
 		c.gridx=2;
 		c.gridy=1;
-		textField.setColumns(10);
-		add(textField,c);
+		txfNome.setColumns(10);
+		add(txfNome,c);
+		
+		JLabel lblRegione = new JLabel("Regione");
+		c.gridx=1;
+		c.gridy=2;
+		add(lblRegione, c);
+		
+		JTextField txfRegione = new JTextField();
+		txfRegione.setColumns(10);
+		c.gridx=2;
+		c.gridy=2;
+		add(txfRegione,c);
 				
 		JLabel lblProv = new JLabel("Provincia");
 		c.gridx=1;
-		c.gridy=2;
+		c.gridy=3;
 		add(lblProv, c);
 		
-		JTextField textField_1 = new JTextField();
-		textField_1.setColumns(10);
+		JTextField txfProv = new JTextField();
+		txfProv.setColumns(10);
 		c.gridx=2;
-		c.gridy=2;
-		add(textField_1,c);
+		c.gridy=3;
+		add(txfProv,c);
 		
 		JLabel lblComune = new JLabel("Comune");
 		c.gridx=1;
-		c.gridy=3;
+		c.gridy=4;
 		add(lblComune, c);
 		
-		JTextField textField_2 = new JTextField();
-		textField_2.setColumns(10);
+		JTextField txfComune = new JTextField();
+		txfComune.setColumns(10);
 		c.gridx=2;
-		c.gridy=3;
-		add(textField_2,c);
+		c.gridy=4;
+		add(txfComune,c);
 		
 		JLabel lblCAP = new JLabel("CAP");
 		c.gridx=1;
-		c.gridy=4;
+		c.gridy=5;
 		add(lblCAP, c);
 		
-		JTextField textField_3 = new JTextField();
-		textField_3.setColumns(10);
+		JTextField txfCAP = new JTextField();
+		txfCAP.setColumns(10);
 		c.gridx=2;
-		c.gridy=4;
-		add(textField_3,c);
+		c.gridy=5;
+		add(txfCAP,c);
 		
 		JLabel lblIndir = new JLabel("Indirizzo");
 		c.gridx=1;
-		c.gridy=5;
+		c.gridy=6;
 		add(lblIndir, c);
 		
-		JTextField textField_4 = new JTextField();
-		textField_4.setColumns(10);
+		JTextField txfIndirizzo = new JTextField();
+		txfIndirizzo.setColumns(10);
 		c.gridx=2;
-		c.gridy=5;
-		add(textField_4,c);
+		c.gridy=6;
+		add(txfIndirizzo,c);
 		
 		
 		JLabel lblTipo = new JLabel("Tipo");
 		c.gridx=1;
-		c.gridy=6;
+		c.gridy=7;
 		add(lblTipo, c);
 		
-		JList list = new JList();
+		JList lstTipo = new JList(tipi);
 		c.gridx=2;
-		c.gridy=6;
-		add(list,c);
+		c.gridy=7;
+		add(lstTipo,c);
 		
-		JButton btnNewButton_1 = new JButton("Registra");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JLabel lblErrori = new JLabel("Nessun errore");
+		c.gridx=1;
+		c.gridy=8;
+		c.gridwidth=4;
+		add(lblErrori, c);
+		
+		JButton btnRegistra = new JButton("Registra");
+		btnRegistra.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-					//funzione registra
-					CardLayout cardLayout = (CardLayout) contentPane.getLayout();
-					cardLayout.show(contentPane,"finalPage");					
+					try {
+						if(stub.nomeCVTaken(txfNome.getText())) {
+							lblErrori.setText("Nome già in uso");
+							return;
+						}
+						if(stub.inserisciCV(txfNome.getText(), txfIndirizzo.getText(), (int) Integer.parseInt(txfCAP.getText()), txfComune.getText(), txfProv.getText(), txfRegione.getText(), (String) lstTipo.getSelectedValue())) {
+							txfNome.setText("");
+							txfIndirizzo.setText("");
+							txfCAP.setText("");
+							txfComune.setText("");
+							txfProv.setText("");
+							txfRegione.setText("");
+							lstTipo.setSelectedIndex(0);
+							lblErrori.setText("Nessun errore");
+							
+							CardLayout cardLayout = (CardLayout) contentPane.getLayout();
+							cardLayout.show(contentPane,"finalPage");
+						}
+						else {
+							lblErrori.setText("Errore di registrazione");
+						}
+					} catch (NumberFormatException | RemoteException | SQLException e1) {}		
 				}
 			}
 		);
 		c.gridx=2;
-		c.gridy=7;		
-		add(btnNewButton_1,c);
+		c.gridy=9;		
+		add(btnRegistra,c);
 		
-		JLabel lblNewLabel_2 = new JLabel("Errori");
-		c.gridx=1;
-		c.gridy=8;
-		c.gridwidth=4;
-		add(lblNewLabel_2, c);
+		
 	}
 }
